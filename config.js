bc5beb39aa1b9f372e109c1873c72ba74a995112
Himasopn <129391898+Himasopn@users.noreply.github.com>
2025-01-31 21:55:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || 'levanter_31a8e9a30762544ec5bf5913b400f1ba65').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
